2b3c96dd582719ed65b2a20c661ca7f3dfb43d74
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-10-31 16:32:14 +0300

    update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -25,7 +25,7 @@ async def start(bot: Client, msg: Message):
                     InlineKeyboardButton(text="- بـدء استخـراج الجلسـة .", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("- قنـاة السـورس .", url="https://t.me/kaido_q"),
+                    InlineKeyboardButton("- قنـاة السـورس .", url="https://t.me/Tepthon"),
                     InlineKeyboardButton("- المطـور .", user_id=OWNER_ID)
                 ]
             ]
