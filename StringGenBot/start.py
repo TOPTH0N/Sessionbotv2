abe0f9beb5f38b2f58cf52c7ff1ef8ee2c8c6c3b
TOPTH0N <119265616+TOPTH0N@users.noreply.github.com>
2023-11-07 19:13:15 +0300

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -18,14 +18,14 @@ async def start(bot: Client, msg: Message):
 **- إذا كنـت تريـد أن يكون حسـابك في أمـان تام فاختر بايروجـرام أمـا إذا كـان رقمك حقيقـي فاختر تيرمـكس .**
 ** - ملاحظـة :**
 **- احـذر مشاركـة الكود لأحـد لأنه يستطيـع اختراق حسـابك ⚠️ .**
-المطـور : [محمد](tg://user?id={OWNER_ID}) !""",
+المطـور : [احمد](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="- بـدء استخـراج الجلسـة .", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("- قنـاة السـورس .", url="https://t.me/Tepthon"),
+                    InlineKeyboardButton("- قنـاة السـورس .", url="https://t.me/zqqqzq"),
                     InlineKeyboardButton("- المطـور .", user_id=OWNER_ID)
                 ]
             ]
